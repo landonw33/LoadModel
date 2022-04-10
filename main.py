eadd18a48bb23b655d4182bf8a8450e0be8a76da
Landon Walker <37057714+landonw33@users.noreply.github.com>
2022-04-09 18:03:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # from table 1, Pwi = Peak Load (%) * Panual
 # from table 2, p_day = Day of week * Pwi
 p_day = 0.93*.862*85
-# from table 3, b is the hourly peak load from 12 am to 12 PM, 24 Hours
+# from table 3, b is the hourly peak load from 12 AM to 12 PM, 24 Hours
 b = [0.67, 0.63, 0.60, 0.59, 0.59, 0.60, 0.74, 0.86, 0.95, 0.95, 0.96, 0.95, 0.95, 0.95, 0.93, 0.94, 0.99, 1, 1, 0.96, 0.91, 0.83, 0.73, 0.63]
 
 x = ()
